4b484000372dbd21eee88bf3b8cc8086572ad1cc
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-11-23 16:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Remove '#' symbol if present
+  shortHex = shortHex.replace('#', '');
+
+  // Check if the length is 3 (short hex code)
+  if (shortHex.length === 3) {
+    // Extend the short hex code to full hex code
+    const fullHex = shortHex
+      .split('')
+      .map(char => char + char) // Duplicate each character
+      .join('');
+    
+    return `#${fullHex.toUpperCase()}`; // Convert to uppercase and add '#'
+  }
+
+  return `Invalid input: ${shortHex}`; // Return an error message for invalid input length
 };
 
-// Do not change the code below.
+// Do not change the code below. 
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
